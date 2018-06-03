ed22ec7ca3a2c3f7fbf2f450afcb01af9d822841
yuta-ushijima <register@yuta-u.com>
2018-06-03 14:47:52 +0900

    エスケープ

---
+++ chapter03/ex03-07-1/main.js
@@ -0,0 +1,6 @@
+const dialog1 = "He looked up and said \"don't do that!\" to Max.";
+const dialog2 = 'He looked up and said "don\'t do that!" to Max.';
+const dialog3 = `He looked up and said "don't do that!" to Max.`;
+console.log(dialog1);
+console.log(dialog3);
+console.log(dialog2 === dialog3);
