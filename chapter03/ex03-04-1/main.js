7d9352b17765fe4de7a8daa506c48351a1a7d99c
yuta-ushijima <register@yuta-u.com>
2018-06-03 14:36:53 +0900

    プリミティブ型とオブジェクト

---
+++ chapter03/ex03-04-1/main.js
@@ -0,0 +1,6 @@
+let room1 = "会議室A";
+console.log(room1);
+let currentRoom = room1;
+console.log(currentRoom);
+/*以下の「会議室A」はダブルクォーテーションで囲まれていないのでリテラルでもないし、宣言されていないので変数でも定数でもない。*/
+let nextRoom  = 会議室A;
