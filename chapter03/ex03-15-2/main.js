a9330df5ecefe351225d43c42e4e4cfb2c2d7cfc
yuta-ushijima <register@yuta-u.com>
2018-06-12 22:57:32 +0900

    日時

---
+++ chapter03/ex03-15-2/main.js
@@ -0,0 +1,5 @@
+const halloween = new Date(2018, 09, 31);
+console.log(halloween);
+
+const halloweenParty = new Date(2018, 9, 31, 19, 0);
+console.log(halloweenParty);
---
+++ chapter03/ex03-15-3/main.js
@@ -0,0 +1,11 @@
+const halloweenParty = new Date(2018, 9, 31, 19, 0);
+
+console.log(halloweenParty.getFullYear());
+console.log(halloweenParty.getMonth());
+console.log(halloweenParty.getDate());
+console.log(halloweenParty.getDay());//曜日。0は日曜日。戻り値が3なので水曜日になる。
+console.log(halloweenParty.getHours());
+console.log(halloweenParty.getMinutes());
+console.log(halloweenParty.getSeconds());
+console.log(halloweenParty.getMilliseconds());
+
